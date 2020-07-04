65b245d35e400411c3bc32918dce7b8287f23837
kimjinkyum <kimo1113@daum.net>
2020-07-05 02:25:42 +0900

    Modify path

--- Deepvoice for english/main.py
+++ Deepvoice for english/main.py
@@ -6,9 +6,9 @@ sys.path.append(path)
 
 import deepvoice3
 deepvoice3.synthesis(
-    checkpoint_path='./tts_model.pth', 
+    checkpoint_path='./source/tts/model.pth', 
     preset='./deepvoice3_vctk2.json', 
-    dst_dir='./output/tts',
-    srt_path='./subtitle.srt',
-    face_path='./res.csv'
+    dst_dir='./output/tts/persub',
+    srt_path='./source/tts/original.txt',
+    face_path='./output/detection/result.csv'
 )
